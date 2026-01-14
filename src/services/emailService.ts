a18225b959c0553dd9503d4a425884e4f95f5efb
ayushmanCodes2004 <ayushman678@gmail.com>
2026-01-15 02:02:00 +0530

    New template id

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -3,7 +3,7 @@ import emailjs from '@emailjs/browser';
 // EmailJS Configuration
 const EMAILJS_SERVICE_ID = 'service_0imi2rl';
 const EMAILJS_PUBLIC_KEY = 'i97hERR272deTG2i4';
-const WELCOME_TEMPLATE_ID = 'template_4nzdapf';
+const WELCOME_TEMPLATE_ID = 'template_o93jfxm';
 
 // Initialize EmailJS with public key
 if (typeof window !== 'undefined') {
